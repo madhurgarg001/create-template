4f06820b6d0701850b1d7ef9b4b496484d8d4466
Madhur Garg <madhur@minewhat.com>
2016-07-26 10:48:06 +0530

    modified list-categories.js

--- www/app/list-categories.js
+++ www/app/list-categories.js
@@ -9,23 +9,27 @@ define([], function () {
         },
 
         getCategoryData : function (CB) {
-            console.log(this)
-            this.categoryData = this._fetchData(CB);
-            if(this.categoryData){
-                
+            console.log(CB(this._fetchData()));
+            if(this.categoryData) {
+                 CB(this.categoryData)
+            }else {
+                this._fetchData(function (data) {
+                    // this.categoryData = this._transform(data);
+                    CB(this.categoryData)
+                })
             }
         },
 
-        _transform: function (catArray) {
-
-            console.log(catArray);
-            var catObj = {};
-            catArray.forEach(function (item) {
-                var name = item.name;
-                catObj[name] = item.sname;
-            });
-            return catObj;
-        }
+        // _transform: function (catArray) {
+        //
+        //     console.log(catArray);
+        //     var catObj = {};
+        //     catArray.forEach(function (item) {
+        //         var name = item.name;
+        //         catObj[name] = item.sname;
+        //     })
+        //     return catObj;
+        // }
 
     }
 });
