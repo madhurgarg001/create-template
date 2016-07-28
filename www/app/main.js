aef3d45de0eeb34fe3ad3f30177c51cc9fb64b9a
Madhur Garg <madhur@minewhat.com>
2016-07-28 18:09:03 +0530

    rank based search

--- www/app/main.js
+++ www/app/main.js
@@ -12,7 +12,7 @@ define(function (require) {
     var singleProduct = require('./single-product');
     var template = require('./_templates/_templates');
     var product = require('./getUrlParams');
-    var searchObj = require('./searchProducts')
+    var searchObj = require('./searchAlgo');
     // Load library/vendor modules using
     // full IDs, like:
     var $ = require('jquery');
